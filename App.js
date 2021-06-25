7818e4f20a8c505f57e3e31cc6ff1f69399abe3d
April <goapril@yahoo.com>
2021-06-25 15:31:57 +0400

    Imported GiftedChat

--- App.js
+++ App.js
@@ -11,6 +11,8 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import { GiftedChat } from 'react-native-gifted-chat'
+
 // Create the navigator
 const Stack = createStackNavigator();
 
